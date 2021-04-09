a60037eb7e31a607c8a05f04977afa027ff636a0
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-09 09:20:35 -0500

    Task Done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2dArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid =
+            {
+                {1,2 },
+                {3,4 },
+                {5,6 },
+            };
+
+            Console.WriteLine(numberGrid[1,0]);
+
+
+
+
+        }
+    }
+}
